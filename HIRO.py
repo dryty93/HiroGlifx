d58c371dc0ad8b289ce885cf01ac0860ef230fb4
dryty93 <dryden.inc718@gmail.com>
2018-05-19 02:10:00 -0400

    editing and cleaning up code. Now variables and the corresponding values are stored within the dictionary varDict.

--- HIRO.py
+++ HIRO.py
@@ -1,6 +1,11 @@
 from simpleLibrary import *
+from HIROTEST import getType
 
-
+fEquationList = []
+numList = []
+strList = []
+isString = 1 > 33
+isNum = 1 > 333
 VARS = []
 NUMS = []
 STRINGS = []
@@ -9,8 +14,9 @@ symbols = []
 addition = []
 subtraction = []
 varVals = []
-fVarNamesList = []
+fVarName = []
 EVAL = ['+', '-', '*', '/']
+dataTypes = {"$":"STRING", "#":"NUM"}
 varNamesFinal = []
 counter = 0
 h = []
@@ -24,31 +30,58 @@ tokens = {'!': False,
           }
 
 keywords = ['WRITE', 'TYPES']
-
+counter = 1
+varDict = {}
 with open('HIRO.glif','r') as readFile:
-    
+
     for words in readFile:
+
+        VARS.append(words)
+
+        if 'WRITE' in words:
+            numList = []
+            writing()
+
+        def writing():
+            writeSequence = words.split('WRITE')
+            writeStringJoin = ''.join(writeSequence)
+            fWriteSequence = writeStringJoin.split('(')
+            o = ''.join(fWriteSequence)
+            fWrite = o.split(')')
+            finalWriter = ''.join(fWrite)
+            write(finalWriter)
+            for everything in varDict:
+                if everything in finalWriter:
+                    write(varDict[everything])
+
+
+        if '!' in words:
+            bool()
+
+        def bool():
+            tokens['!'] = True
+
+            if tokens:
+                print('conditional')
+            else:
+                print('no')
+        for items in VARS:
+            if "#" in items:
+                isString = False
+                isNum = True
+                numList.append(items)
+            if "$" in items:
+                isString = True
+                isNum = False
+                strList.append(items)
+        if '+' or '-' or '/' or '*' in words:
+            getType()
+
+
         counter += 1
-        w = words.join('#=')
-        if w in words:
-            nw = words.split(-3)
-            
-            write(nw)
-        else:
-            write('do better stupid')
+        w = words.split('VAR')
+        varDict[str(w[0])] = str(w[-1])
 
-#open file and read it. if any of the words in file match token perform function
-#save var names and vals as dictionary to call each of them.
-
-
-
-
-
-
-
-
-
-
-
-
+        varNames =str(varDict.keys())
+        varValues = str(varDict.values())
 
